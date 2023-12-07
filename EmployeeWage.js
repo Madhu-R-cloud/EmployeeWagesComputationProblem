c5fa3af97007c6acd0cc760255f5c706d0d81a11
Madhu-R-cloud <madhusudanr00@gmail.com>
2023-12-07 15:46:39 +0530

    ADD: UC2 - Calculate Daily employee wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,30 +2,16 @@
 
 const readline = require('readline-sync');
 
-let Employee = [{
-    Eid: 1,
-    Name: "John Doe",
-    Attendance: '', 
-    WorkHour: 8,
-    DailyWage: 20,
-},{
-    Eid: 1,
-    Name: "Madhu",
-    Attendance: '', 
-    WorkHour: 8,
-    DailyWage: 20,
-},{
-    Eid: 1,
-    Name: "Manu",
-    Attendance: '', 
-    WorkHour: 8,
-    DailyWage: 20,
-},
-]
+    const Employees = [
+        { Eid: 1, Name: "Madhu", Attendance: '', DailyWage: 0, EmployeeType : ''},
+        { Eid: 2, Name: "Manu", Attendance: '', DailyWage: 0,EmployeeType : ''},
+        { Eid: 3, Name: "Smith", Attendance: '', DailyWage: 0, EmployeeType : ''},
+    ];
+
 
 function employee(){
 while(true){
-    const choice = parseInt(readline.question("Enter your Choice\n1.Check Employee Present OR Absent\n2. Calculate Daily Wages\n3. Add Parttime Employee and wage\n4. Calculate wages per month\n4. Exit\n"));
+    const choice = parseInt(readline.question("Enter your Choice\n1.Check Employee Present OR Absent\n2. Calculate Daily Wages\n3. Add Partime Employee and wage\n4. Calculate wages per month\n4. Exit\n"));
     switch(choice){
         case 1: const randomValue = Math.round(Math.random())
         
@@ -38,6 +24,7 @@ while(true){
                 break;
         
         case 2:
+            workHrDailyWage();
             break;
         case 3:
             break;
@@ -50,10 +37,29 @@ while(true){
     
 }
 
-function workHrDailyWage(){
-    const workHour = 8;
-    const dailyWagePerHr = 20;
-   const totalWage = workHour * dailyWagePerHr;
+function workHrDailyWage() {
+    const hourlyWage = 20;
+
+    console.log("|-----|-----------|--------------|------------|");
+    console.log("| Eid | Name      | Attendance   | Daily Wage |");
+    console.log("|-----|-----------|--------------|------------|");
+
+    Employees.forEach((employee) => {
+        const randomValue = Math.round(Math.random());
+
+        if (randomValue == 1) {
+            employee.Attendance = 'Present';
+            employee.DailyWage = 8 * hourlyWage;
+        } else {
+            employee.Attendance = 'Absent';
+            employee.DailyWage = 0;
+        }
+
+        console.log(`| ${employee.Eid}   | ${employee.Name.padEnd(9)} | ${employee.Attendance.padEnd(12)} | $${employee.DailyWage.toString().padEnd(9)} |`);
+    });
+
+    console.log("|-----|-----------|--------------|------------|");
 }
+
 
 employee();
